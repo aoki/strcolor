97a2d17126880c9f69bc8a882870318a3fa3a15e
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-07-21 13:19:17 +0900

    Fix issues

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 [![Build Status](https://travis-ci.org/ringohub/strcolor.svg?branch=master)](https://travis-ci.org/ringohub/strcolor)
+[![Coverage Status](https://coveralls.io/repos/github/ringohub/strcolor/badge.svg?branch=master)](https://coveralls.io/github/ringohub/strcolor?branch=master)
+[![Code Climate](https://codeclimate.com/github/ringohub/strcolor/badges/gpa.svg)](https://codeclimate.com/github/ringohub/strcolor)[![Issue Count](https://codeclimate.com/github/ringohub/strcolor/badges/issue_count.svg)](https://codeclimate.com/github/ringohub/strcolor)
 
--- Strcolor.js
+++ Strcolor.js
@@ -8,15 +8,20 @@ var Strcolor = {
     var hash = crypto.createHash('md5').update(str).digest('hex');
 
     // MaterialColorには16種類のプライマリカラーがあるため、MD5した前半16文字を加算し、Modをとり色相を決定する
-    var hue = hash.substr(0, 16).split('').reduce(function(acc, n){ return (acc + parseInt(n, 16)) % 16;}, 0);
+    var hue = reduceFunc(hash, 0, 16);
 
     // 次に、明度・彩度を12段階の中から決定する
-    var level = hash.substr(16, 16).split('').reduce(function(acc, n){ return (acc + parseInt(n, 16)) % 12;}, 0);
+    var level = reduceFunc(hash, 16, 12);
 
     var color = material[hue].value[level].code;
 
     return color;
   }
 };
+
+function reduceFunc(hash, start, mod) {
+  return hash.substr(start, 16).split('').
+    reduce(function(acc, n){ return (acc + parseInt(n, 16)) % mod;}, 0);
+}
 
 module.exports = Strcolor;
